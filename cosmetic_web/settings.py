77bd2ac7d88e5f30cc3d9c9e48e6492b9a47c80a
Lucas <qq838126232@163.com>
2019-11-23 16:12:27 +0800

    整理静态文件

--- cosmetic_web/settings.py
+++ cosmetic_web/settings.py
@@ -123,11 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),
-                    # '/var/www/cosmetic_web/static/',
-                    ]   # static文件夹路径
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),
+#                     # '/var/www/cosmetic_web/static/',
+#                     ]   # static文件夹路径
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # 项目设置中设置接入MongoDB数据库系统
 from mongoengine import connect
